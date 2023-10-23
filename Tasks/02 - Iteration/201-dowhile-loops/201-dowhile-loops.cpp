7ca8db391275b663e28296ce565dac79ffc65d91
James Alexander Hayward <146201043+JamesAlexanderHayward@users.noreply.github.com>
2023-10-23 15:54:14 +0100

    UPdate

--- Tasks/02 - Iteration/201-dowhile-loops/201-dowhile-loops.cpp
+++ Tasks/02 - Iteration/201-dowhile-loops/201-dowhile-loops.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-    int a = 0
+    int a = 5
         cout << "Start while loop" << endl;
 
     do
     {
         cout << "a=" << a;
         a = a--;
-    } while a <= 0;
+    } while a => 0;
     
 }
 
