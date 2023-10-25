28b18d1ba8aca8fbce1faa899c7168e4868ef1b5
James Alexander Hayward <146201043+JamesAlexanderHayward@users.noreply.github.com>
2023-10-25 23:40:16 +0100

    Homeowkr

--- Tasks/05 - Functions/Functions/501-function/501-function.cpp
+++ Tasks/05 - Functions/Functions/501-function/501-function.cpp
@@ -3,28 +3,30 @@ using namespace std;
 
 int L;		//This is not an ideal location - we will soon address this
 
+void displayBanner()
+{
+	for (unsigned int n = 0; n < (L + 4); n++) {
+		cout << "*";
+	}
+	cout << endl;
+}
+
 int main()
 {
-	const string message = "Welcome to Computer Science";
+	const string message = "Welcome to Computer Science, comp1000 ";
 	
 	//Get the length of the string
 	L = message.length();
 
 	//Write the banner above
-	for (unsigned int n = 0; n < (L + 4); n++) {
-		cout << "*";
-	}
-	cout << endl;
+	displayBanner();
 
 	cout << "* ";				//Leading *
 	cout << message;			//Message
 	cout << " *" << endl;		//Trailing *
 
 	//Write the banner below
-	for (unsigned int n = 0; n < (L + 4); n++) {
-		cout << "*";
-	}
-	cout << endl;
+	displayBanner()
 
 	//Tell the calling shell all is well
 	return 0;
